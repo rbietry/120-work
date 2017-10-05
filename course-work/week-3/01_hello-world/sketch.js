f30e8da85b0f5c613983814ba8798fccd65e5132
Rachel Bietry <rbietry@hotmail.com>
2017-10-04 23:47:21 -0600

    created week 3 homework with a canvas, background, and text

---
+++ course-work/week-3/01_hello-world/sketch.js
@@ -0,0 +1,3 @@
+function setup(){
+    text("Hello Rachel!", 0, 10);
+}
---
+++ hw-3/sketch.js
@@ -0,0 +1,17 @@
+// declare the setup function
+function setup() {
+
+    // create a canvas 500px by 500px
+    createCanvas( 500, 500 );
+
+    // color the background 'green'
+    background( 'green' );
+}
+
+// declare the draw function
+function draw() {
+
+    // tell p5 to print the text string "Hello friends!"
+    // this will be positioned at ( x: 50, y: 100 ).
+    text( "Hello friends!", 50, 100 );
+}
